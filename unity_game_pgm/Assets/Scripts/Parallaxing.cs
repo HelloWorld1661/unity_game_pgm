b3c7057533a9860ba08bb95a3781f72851ab7a6f
Ruslan <Ruslan@arpiseaQs-MacBook-Pro.local>
2017-10-18 18:32:42 -0400

    cam fixes, BG tiling, player responsiveness, music, extra coins

---
+++ unity_game_pgm/Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour {
+
+	public Transform[] bgs;					// bgs to Behaviour parallaxed
+	private float[] parallaxScales;			// proportion of cam's mvmt to mv bgs by
+	private float smoothing = 1f; 			// set above 0
+
+	private Transform cam;					// ref to main cam's transform
+	private Vector3 prevCamPos; 			// 3 for xyz vals - stores position of cam in prev frame
+
+	/* Awake is called before Start()
+	 * but after gameObj are setup
+	 * great for references
+	 */
+	void Awake() {
+		cam = Camera.main.transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		prevCamPos = cam.position;
+
+		parallaxScales = new float[bgs.Length];
+
+		// assigning coressponding parallax scales
+		for (int i=0; i<bgs.Length; i++) {
+			parallaxScales[i] = bgs[i].position.z * -1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// for each bg
+		// parallax is opp of cam mvmt to create effect
+		for (int i=0; i<bgs.Length; i++) {
+			float parallax = (prevCamPos.x - cam.position.x) * parallaxScales[i];
+
+			float bgTargetPosX = bgs[i].position.x + parallax;
+
+			Vector3 bgTargetPos = new Vector3 (bgTargetPosX, bgs[i].position.y, bgs[i].position.z);
+
+			// fade b/w curr & target pos using Lerp
+			// Time.deltaTime converts frames to seconds
+			bgs[i].position = Vector3.Lerp(bgs[i].position, bgTargetPos, smoothing * Time.deltaTime);
+		}
+		prevCamPos = cam.position;
+	}
+}
---
+++ unity_game_pgm/Assets/Scripts/Tiling.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// require / call for component - if not there, unity will create it
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour {
+
+	public int offsetX = 2;
+
+	// checking if we need to instantiate things
+	public bool hasRightBuddy = false;
+	public bool hasLeftBuddy = false;
+
+	// used if obj not tileable
+	public bool revScale = false;
+
+	// el width
+	private float spriteWidth = 0f;
+	private Camera cam;
+	private Transform myTransform;
+
+	void Awake() {
+		cam = Camera.main;
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sRenderer = GetComponent<SpriteRenderer> ();
+		spriteWidth = sRenderer.sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasLeftBuddy == false || hasRightBuddy == false) {
+			// calc the cam's extend (half width) of what cam can see in world coordinates
+			float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+			// calc the x pos where cam can see the edge of sprite el
+			float edgeVisiblePosRight = (myTransform.position.x + spriteWidth/2) - camHorizontalExtend;
+			// flip arith
+			float edgeVisiblePosLeft = (myTransform.position.x -  spriteWidth/2) + camHorizontalExtend;
+
+			// checking if we can see edge of el
+			// then calling MakeNewBuddy func if needed
+			if (cam.transform.position.x >= edgeVisiblePosRight - offsetX && hasRightBuddy == false) {
+				MakeNewBuddy (1); // 1 b/c we want it on the right
+				hasRightBuddy = true;
+			} else if (cam.transform.position.x <= edgeVisiblePosLeft + offsetX && hasLeftBuddy == false) {
+				MakeNewBuddy (-1);
+				hasLeftBuddy = true;
+			}
+		}
+	}
+
+	// makes new buddy on required side
+	void MakeNewBuddy(int rightOrLeft) {
+		// calc new pos for new buddy
+		Vector3 newPos = new Vector3(myTransform.position.x + spriteWidth * rightOrLeft, myTransform.position.y, myTransform.position.z);
+		// myTransform is clone of obj
+		// instantitaing and storing new buddy in Transform obj
+		Transform newBuddy = (Transform)Instantiate(myTransform, newPos, myTransform.rotation); // or we can typecast with <as Transform;>
+
+		// if not tileable rev x to smooth over seams
+		if (revScale == true) {
+			newBuddy.localScale = new Vector3 (newBuddy.localScale.x * -1, newBuddy.localScale.y, newBuddy.localScale.z);
+		}
+
+		newBuddy.parent = myTransform.parent;
+		if (rightOrLeft > 0) {
+			newBuddy.GetComponent<Tiling> ().hasLeftBuddy = true;
+		} else {
+			newBuddy.GetComponent<Tiling> ().hasRightBuddy = true;
+		}
+	}
+}
