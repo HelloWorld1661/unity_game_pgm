5f297b3e6ae853febafecdcccd655c30ce822112
Ruslan <Ruslan@arpiseaQs-MacBook-Pro.local>
2017-10-18 22:19:44 -0400

    cam stays put when player dies

--- unity_game_pgm/Assets/Scripts/SimpleCamera2DFollow.cs
+++ unity_game_pgm/Assets/Scripts/SimpleCamera2DFollow.cs
@@ -16,7 +16,7 @@ public class SimpleCamera2DFollow : MonoBehaviour
 		Vector3 position = transform.position;
 		position.x = target.transform.position.x + 5f;
 		// RP 10/18/17 this sets a cam ceiling so it doesn't go past BG height
-		if (target.position.y < 4) {
+		if (target.position.y < 4 && target.position.y > 0) {
 			position.y = target.transform.position.y;
 		}
 		transform.position = position;
