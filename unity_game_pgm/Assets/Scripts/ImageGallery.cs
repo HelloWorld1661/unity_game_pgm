67f40f0bc84b5a479305ffa681d4628724c69007
Ruslan Pantaev <rpantaev@gmail.com>
2017-11-07 05:15:29 -0500

    image gallery script

---
+++ unity_game_pgm/Assets/Scripts/ImageGallery.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ImageGallery : MonoBehaviour {
+
+
+	// apply this script on a sprite object (RP)
+	public Sprite[] gallery; 
+	public Button nextImg;
+	public Button prevImg;
+	public int i = 0;
+
+	void Awake () {
+		// init
+		this.GetComponent<SpriteRenderer>().sprite = gallery[0];
+	}
+
+	public void BtnNext (){
+		if(i + 1 < gallery.Length){
+			i++;
+		}
+	}
+
+	public void BtnPrev () {
+		if (i - 1 > 0){
+			i --;
+		}
+	}
+	void Update () {
+		this.GetComponent<SpriteRenderer>().sprite = gallery[i];
+	}
+}
