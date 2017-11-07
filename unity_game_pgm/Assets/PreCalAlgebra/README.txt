da1fef5dd408ce2662936e38f3ad9f5088aeadd8
Ruslan Pantaev <rpantaev@gmail.com>
2017-11-07 07:33:45 -0500

    most of tutorial scenes done

--- unity_game_pgm/Assets/PreCalAlgebra/README.txt
+++ unity_game_pgm/Assets/PreCalAlgebra/README.txt
@@ -12,6 +12,6 @@ Pullman, Wa. 99164-3113
 # it is good b/c easy to parse
 # <http://www.math.wsu.edu/HS/problems.html>
 # Display screenshots by date/time to see correct order
-# NULL folders were duplicates, so I nullified them
+#
 # in the future if needed, *.png can be made transparent (gimp/PS)
 # 	-add alpha channel -use color picker to select bg -delete bg -export
--- unity_game_pgm/Assets/Scripts/ImageGallery.cs
+++ unity_game_pgm/Assets/Scripts/ImageGallery.cs
@@ -11,22 +11,35 @@ public class ImageGallery : MonoBehaviour {
 	public Sprite[] gallery; 
 	private int i = 0;
 
-	void Awake () {
+	void Awake() {
 		// init
 		this.GetComponent<SpriteRenderer>().sprite = gallery[0];
 	}
 
-	public void BtnNext (){
-		if(i + 1 < gallery.Length){
-			i++;
-		}
+	public void BtnNext(){
+		if (i + 1 < gallery.Length) { i++; }
 		this.GetComponent<SpriteRenderer>().sprite = gallery[i];
 	}
 
-	public void BtnPrev () {
-		if (i > 0){
-			i --;
-		}
+	public void BtnPrev() {
+		if (i > 0) { i--; }
 		this.GetComponent<SpriteRenderer>().sprite = gallery[i];
+	}
+
+	void Update() {
+		// TODO fix this, for some reason not capturing input...
+
+		// capture input for keydown L and R
+//		if (Input.GetKeyDown("right") && (i + 1 < gallery.Length)) {
+//			i++;
+//			this.GetComponent<SpriteRenderer>().sprite = gallery[i];
+//		} else if (Input.GetKeyDown("left") && (i > 0)) {
+//			i--;
+//			this.GetComponent<SpriteRenderer>().sprite = gallery[i];
+//		}
+
+//		if (Input.GetKeyDown ("right")) {
+//			Debug.Log ("rightKeyPressed");
+//		}
 	}
 }
