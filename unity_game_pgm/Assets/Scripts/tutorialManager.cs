5d1d68738678040993ee2244161af49f24e93b57
Ruslan Pantaev <rpantaev@gmail.com>
2017-11-07 08:19:46 -0500

    tutorials working

---
+++ unity_game_pgm/Assets/Scripts/tutorialManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class tutorialManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// (RP)
+	public void ChooseTutorial() {
+		int chosenInt = GameObject.Find ("TopicDropdown").GetComponent<Dropdown>().value;
+		string dest = "tut_" + chosenInt;
+		SceneManager.LoadScene(dest);
+	}
+}
