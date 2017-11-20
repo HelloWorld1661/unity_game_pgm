7a7ecfbd87208421dce5c4a6f1563ba7ad6faf71
Ruslan Pantaev <rpantaev@gmail.com>
2017-11-20 16:41:22 -0500

    TODO if we have time (GameManager)

---
+++ unity_game_pgm/Assets/Scripts/DataAccess.cs
@@ -0,0 +1,82 @@
+﻿//using System;
+//using System.IO;
+//using System.Runtime.InteropServices;
+//using System.Runtime.Serialization.Formatters.Binary;
+//using UnityEngine;
+//
+//public class DataAccess
+//{
+//	[DllImport("__Internal")]
+//	private static extern void SyncFiles();
+//
+//	[DllImport("__Internal")]
+//	private static extern void WindowAlert(string message);
+//
+//	public static void Save(GameDetails gameDetails)
+//	{
+//		string dataPath = string.Format("{0}/GameDetails.dat", Application.persistentDataPath);
+//		BinaryFormatter binaryFormatter = new BinaryFormatter();
+//		FileStream fileStream;
+//
+//		try
+//		{
+//			if (File.Exists(dataPath))
+//			{
+//				File.WriteAllText(dataPath, string.Empty);
+//				fileStream = File.Open(dataPath, FileMode.Open);
+//			}
+//			else
+//			{
+//				fileStream = File.Create(dataPath);
+//			}
+//
+//			binaryFormatter.Serialize(fileStream, gameDetails);
+//			fileStream.Close();
+//
+//			if (Application.platform == RuntimePlatform.WebGLPlayer)
+//			{
+//				SyncFiles();
+//			}
+//		}
+//		catch (Exception e)
+//		{
+//			PlatformSafeMessage("Failed to Save: " + e.Message);
+//		}
+//	}
+//
+//	public static GameDetails Load()
+//	{
+//		GameDetails gameDetails = null;
+//		string dataPath = string.Format("{0}/GameDetails.dat", Application.persistentDataPath);
+//
+//		try
+//		{
+//			if (File.Exists(dataPath))
+//			{
+//				BinaryFormatter binaryFormatter = new BinaryFormatter();
+//				FileStream fileStream = File.Open(dataPath, FileMode.Open);
+//
+//				gameDetails = (GameDetails)binaryFormatter.Deserialize(fileStream);
+//				fileStream.Close();
+//			}
+//		}
+//		catch (Exception e)
+//		{
+//			PlatformSafeMessage("Failed to Load: " + e.Message);
+//		}
+//
+//		return gameDetails;
+//	}
+//
+//	private static void PlatformSafeMessage(string message)
+//	{
+//		if (Application.platform == RuntimePlatform.WebGLPlayer)
+//		{
+//			WindowAlert(message);
+//		}
+//		else
+//		{
+//			Debug.Log(message);
+//		}
+//	}
+//}
---
+++ unity_game_pgm/Assets/Scripts/GameManager.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;       //Allows us to use Lists. 
+using System;
+
+public class GameManager : MonoBehaviour
+{
+
+//	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+//	private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+//	private int level = 3;                                  //Current level number, expressed in game as "Day 1".
+//
+//	//Awake is always called before any Start functions
+//	void Awake()
+//	{
+//		//Check if instance already exists
+//		if (instance == null)
+//
+//			//if not, set instance to this
+//			instance = this;
+//
+//		//If instance already exists and it's not this:
+//		else if (instance != this)
+//
+//			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+//			Destroy(gameObject);    
+//
+//		//Sets this to not be destroyed when reloading scene
+//		DontDestroyOnLoad(gameObject);
+//
+//		//Get a component reference to the attached BoardManager script
+//		boardScript = GetComponent<BoardManager>();
+//
+//		//Call the InitGame function to initialize the first level 
+//		InitGame();
+//	}
+//
+//	//Initializes the game for each level.
+//	void InitGame()
+//	{
+//		//Call the SetupScene function of the BoardManager script, pass it current level number.
+//		boardScript.SetupScene(level);
+//
+//	}
+//
+//
+//
+//	//Update is called every frame.
+//	void Update()
+//	{
+//
+//	}
+//
+
+
+
+
+	// see <http://amalgamatelabs.com/Blog/2016/Data_Persistence/>
+
+	[Serializable]
+	public class GameDetails
+	{
+		public decimal XP;
+		public decimal XPMultiplier;
+		public decimal NextXPLevelUp;
+		public int DoubleXPCost;
+		public int CurrentLevel;
+		public int ClickCount;
+		public int BrainCount;
+		public int CurrentGun;
+		public int AutomationLevel;
+		public DateTime? SaveDate;
+		public bool ProgressBarIsFull;
+
+		public GameDetails()
+		{
+			XP = 0.0M;
+			XPMultiplier = 1.0M;
+			NextXPLevelUp = 100.0M;
+			DoubleXPCost = 2000;
+			CurrentLevel = 1;
+			ClickCount = 0;
+			BrainCount = 0;
+			CurrentGun = 0;
+			AutomationLevel = 0;
+			SaveDate = null;
+			ProgressBarIsFull = false;
+		}
+
+		public void IncrementClickCount()
+		{
+
+		}
+
+		public void LevelUp()
+		{
+
+		}
+
+		public void AutomateClicksBetweenSessions()
+		{
+
+		}
+	}
+}
