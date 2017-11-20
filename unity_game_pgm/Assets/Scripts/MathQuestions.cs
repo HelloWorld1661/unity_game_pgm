2c37fbe0040f2f1c4c894444de0d6548c74aed72
Ruslan Pantaev <rpantaev@gmail.com>
2017-11-20 16:13:08 -0500

    game play improved - TODO

--- unity_game_pgm/Assets/Scripts/MathQuestions.cs
+++ unity_game_pgm/Assets/Scripts/MathQuestions.cs
@@ -2,6 +2,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine;
 //using System; 	// using Unity's Random instead
@@ -14,15 +15,28 @@ public class MathQuestions : MonoBehaviour {
 
 	private int op1;
 	private int op2;
+	private const int OPER_SIZE = 5;
+	private char[] operators = new char[OPER_SIZE];
+	private int operIndex;
 	// create array to store A,B,C,D answer key
 	private const int SIZE = 4;
 	private int[] answers = new int[SIZE];
 
 	private int correctIndex;
 
-	// TODO create array of math operators? [+, -, /, *]
 
-	public void StartTheQuiz()
+	void Awake() {
+		// create/init array of math operators? [+, -, /, *]
+		operators [0] = '+';
+		operators [1] = '-';
+		operators [2] = '/';
+		operators [3] = '*';
+		operators [4] = '%';
+		operIndex = Random.Range(0, OPER_SIZE);
+	}
+
+
+	public void GenerateMath()
 	{
 		// generate random values for operands
 		// TODO replace 1 and 101 vals with variables dependent on difficulty!
@@ -33,31 +47,60 @@ public class MathQuestions : MonoBehaviour {
 		for (int i=0; i<SIZE; ++i) {
 			// we are checking that we don't accidentally / magically
 			//		generate the right answer!
-			answers[i] = Random.Range (0, 202);
-			while (answers[i] == op1 + op2) {
-				answers[i] = Random.Range (0, 202);
+			answers[i] = Random.Range (-1000, 1000);
+			while (answers[i] == PerformOp(op1, op2)) {
+				answers[i] = Random.Range (-1000, 1000);
 			}
 		}
 		// populate rand arr index with Correct answer
 		// 		note: Random.Range(Inclusive val, Exclusive val)
 		correctIndex = Random.Range(0, SIZE);
-		answers[correctIndex] = op1 + op2;
+		answers[correctIndex] = PerformOp(op1, op2);
 		AnswerCoins [correctIndex].GetComponent<AnswerCoin> ().isCorrect = true;
 	}
 
-	// TODO We should call this upon every time the player is finished with the question
-	// 		maybe have a bool listener in update
-	void Start () {
-		StartTheQuiz ();
-		mathQuestion.text = "Evaluate: " + op1 + " + " + op2 + " = ? \n\n" + 
+	private int PerformOp(int a, int b) {
+		int temp = 0;
+		switch (operIndex) {
+			case 0:
+				temp = a + b;
+				break;
+			case 1:
+				temp = a - b;
+				break;
+			case 2:
+				temp = a / b;
+				break;
+			case 3:
+				temp = a * b;
+				break;
+			case 4:
+				temp = a % b;
+				break;
+		}
+		return temp;
+	}
+
+	private void PrintMath() {
+		mathQuestion.text = "Evaluate: " + op1 + " " + operators[operIndex] + " " + op2 + " = ? \n\n" + 
 			"(A) " + answers[0] + 
 			" (B) " + answers[1] +
 			" (C) " + answers[2] +
 			" (D) " + answers[3];
 	}
 
+	// TODO We should call this upon every time the player is finished with the question
+	// 		maybe have a bool listener in update
+	void Start () {
+		GenerateMath();
+		PrintMath ();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		// put if statement with bool listener to trigger question refresh
+		if (AnswerCoins[correctIndex] == null) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
 	}
 }
