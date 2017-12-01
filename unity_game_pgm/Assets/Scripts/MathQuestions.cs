7dea9a0e4d672b282cd6128c2e529a8ede7711f1
Ruslan Pantaev <rpantaev@gmail.com>
2017-12-01 14:49:06 -0500

    improved math logic + square roots added + rand vals now within range of answer to be more realistic

--- unity_game_pgm/Assets/Scripts/MathQuestions.cs
+++ unity_game_pgm/Assets/Scripts/MathQuestions.cs
@@ -16,7 +16,7 @@ public class MathQuestions : MonoBehaviour {
 
 	private float op1;
 	private float op2;
-	private const int OPER_SIZE = 5;
+	private const int OPER_SIZE = 6;
 	private char[] operators = new char[OPER_SIZE];
 	private int operIndex;
 	// create array to store A,B,C,D answer key
@@ -33,6 +33,7 @@ public class MathQuestions : MonoBehaviour {
 		operators [2] = '/';
 		operators [3] = '*';
 		operators [4] = '%';
+		operators [5] = (char)0x221A; 	// sqaure root / radical symbol
 		operIndex = Random.Range(0, OPER_SIZE);
 	}
 
@@ -60,25 +61,38 @@ public class MathQuestions : MonoBehaviour {
 		op1 = Random.Range(1, 101);
 		op2 = Random.Range(1, 101);
 
+		float answer = PerformOp (op1, op2);
+
 		// populate answer key with junk, incorrect answer
-		for (int i=0; i<SIZE; ++i) {
+		for (int i=0; i<SIZE; ++i)
+		{
+			// gen rand values within a range of 10 of the answer
+			if (answer % 1 == 0) { // cast rand vals to int to match real answer
+				answers [i] = Random.Range ((int)answer - 10, (int)answer + 10);
+			} else { // leave as float
+				answers [i] = Random.Range (answer - 10, answer + 10);
+			}
 			// we are checking that we don't accidentally / magically
-			//		generate the right answer!
-			answers[i] = Random.Range (-1000, 1000);
-			while (answers[i] == PerformOp(op1, op2)) {
-				answers[i] = Random.Range (-1000, 1000);
+			//		generate the right answer! Will likely never enter this while loop, but it's here to be safe
+			while (answers[i] == answer) {
+				if (answer % 1 == 0) {
+					answers [i] = Random.Range ((int)answer - 10, (int)answer + 10);
+				} else {
+					answers [i] = Random.Range (answer - 10, answer + 10);
+				}
 			}
 		}
 		// populate rand arr index with Correct answer
 		// 		note: Random.Range(Inclusive val, Exclusive val)
 		correctIndex = Random.Range(0, SIZE);
-		answers[correctIndex] = PerformOp(op1, op2);
+		answers[correctIndex] = answer;
 		AnswerCoins [correctIndex].GetComponent<AnswerCoin> ().isCorrect = true;
 	}
 
 	private float PerformOp(float a, float b) {
 		float temp = 0;
-		switch (operIndex) {
+		switch (operIndex)
+		{
 			case 0:
 				temp = a + b;
 				break;
@@ -94,37 +108,50 @@ public class MathQuestions : MonoBehaviour {
 			case 4:
 				temp = a % b;
 				break;
+			case 5:
+				// for square roots, only op b (op2) will be processed
+				temp = Mathf.Sqrt (b);
+				break;
 		}
 		return temp;
 	}
 
 	private void PrintMath() {
-		mathQuestion.text = "Evaluate: " + op1 + " " + operators [operIndex] + " " + op2 + " = ? \n\n" +
-		"(A) ";
-		// RP 2017-11-27
-		// checking decimal, then use Fraction struct to convert to traditional fraction
-		if (answers [0] % 1 != 0) {
+		// RP 2017-12-1
+		// if square root, then diplay only op2
+		if (operIndex == 5) {
+			mathQuestion.text = "Evaluate: " + operators [operIndex] + op2 + " = ? \n\n" +
+			"(A) ";
+		} else {
+			mathQuestion.text = "Evaluate: " + op1 + " " + operators [operIndex] + " " + op2 + " = ? \n\n" +
+			"(A) ";
+		}
+
+		if (operIndex == 2) { // if performing division, convert to fraction
 			Fraction newFraction = Fraction.Parse ((double)answers [0]);
 			mathQuestion.text += newFraction.Numerator + "/" + newFraction.Denominator;
 		} else {
 			mathQuestion.text += answers [0];
 		}
+
 		mathQuestion.text += " (B) ";
-		if (answers [1] % 1 != 0) {
+		if (operIndex == 2) { // if performing division, convert to fraction
 			Fraction newFraction = Fraction.Parse ((double)answers [1]);
 			mathQuestion.text += newFraction.Numerator + "/" + newFraction.Denominator;
 		} else {
 			mathQuestion.text += answers [1];
 		}
+
 		mathQuestion.text += " (C) ";
-		if (answers [2] % 1 != 0) {
+		if (operIndex == 2) { // if performing division, convert to fraction
 			Fraction newFraction = Fraction.Parse ((double)answers [2]);
 			mathQuestion.text += newFraction.Numerator + "/" + newFraction.Denominator;
 		} else {
 			mathQuestion.text += answers [2];
 		}
+
 		mathQuestion.text += " (D) ";
-		if (answers [3] % 1 != 0) {
+		if (operIndex == 2) { // if performing division, convert to fraction
 			Fraction newFraction = Fraction.Parse ((double)answers [3]);
 			mathQuestion.text += newFraction.Numerator + "/" + newFraction.Denominator;
 		} else {
@@ -143,8 +170,7 @@ public class MathQuestions : MonoBehaviour {
 		/// <summary>
 		/// Constructor
 		/// </summary>
-		public Fraction(int numerator, int denominator)
-		{
+		public Fraction(int numerator, int denominator) {
 			this.Numerator = numerator;
 			this.Denominator = denominator;
 		}
@@ -152,8 +178,7 @@ public class MathQuestions : MonoBehaviour {
 		/// <summary>
 		/// Approximates a fraction from the provided double
 		/// </summary>
-		public static Fraction Parse(double d)
-		{
+		public static Fraction Parse(double d) {
 			return ApproximateFraction(d);
 		}
 
@@ -161,8 +186,7 @@ public class MathQuestions : MonoBehaviour {
 		/// Returns this fraction expressed as a double, rounded to the specified number of decimal places.
 		/// Returns double.NaN if denominator is zero
 		/// </summary>
-		public double ToDouble(int decimalPlaces)
-		{
+		public double ToDouble(int decimalPlaces) {
 			if (this.Denominator == 0)
 				return double.NaN;
 
@@ -187,19 +211,15 @@ public class MathQuestions : MonoBehaviour {
 
 			while (System.Math.Abs(fraction - value) > EPSILON)
 			{
-				if (fraction < value)
-				{
+				if (fraction < value) {
 					n++;
-				}
-				else
-				{
+				} else {
 					d++;
 					n = (int)System.Math.Round(value * d);
 				}
 
 				fraction = n / (double)d;
 			}
-
 			return new Fraction(n, d);
 		}
 	}
@@ -207,8 +227,9 @@ public class MathQuestions : MonoBehaviour {
 	public void UpdateHint()
 	{
 		string temp = "";
-		switch (operIndex) {
-		case 0:
+		switch (operIndex)
+		{
+		case 0:	// +
 			temp = "Use your addition powers! \n\n Try adding the largest left-most values first. \n 910 + 300... do 900 + 300 first, which equals 1200 then add remaining 10!\n\n" +
 				"910 + 300 = 1220\n\n" +
 				"Another method uses rounding: \n" +
@@ -219,20 +240,20 @@ public class MathQuestions : MonoBehaviour {
 				"644 +238 = 882";
 
 			break;
-		case 1:
+		case 1:	// -
 			temp = "If number you're subtracting from is smaller (say 2 - 10), simply make the answer -10 and add back 2... 2 - 10 = -8! FLIP and add!\n\n" +
 				"When subtracting from 1,000...\nHere’s a basic rule to subtract a large number from 1,000: Subtract every number except the last from 9 and subtract the final number from 10\n\nFor example:\n1,000 – 556\n\n" +
 				"Step 1: Subtract 5 from 9 = 4\n\nStep 2: Subtract 5 from 9 = 4\n\nStep 3: Subtract 6 from 10 = 4\n\nThe answer is 444.";
 
 			break;
-		case 2:
+		case 2:	// /
 			temp = "Division tricks\n\nHere’s a quick way to know when a number can be evenly divided by these certain numbers:\n\n" +
 				"10 if the number ends in 0\n9 when the digits are added together and the total is evenly divisible by 9\n8 if the last three digits are evenly divisible by 8 or are 000\n" +
 				"6 if it is an even number and when the digits are added together the answer is evenly divisible by 3\n5 if it ends in a 0 or 5\n4 if it ends in 00 or a two digit number that" +
 				"is evenly divisible by 4\n3 when the digits are added together and the result is evenly divisible by the number 3\n2 if it ends in 0, 2, 4, 6, or 8";
 
 			break;
-		case 3:
+		case 3:	// *
 			temp = "Every multiplication has a twin, which may be easier to remember.\nFor example if you forget 8×2, you might remember 2×8=16. This way, you only have to remember half the table!\n\n\n" +
 				"Another trick is to use base 10, base 20 or base 50.\n" +
 				"Base 10 Example: 15 * 13    So, 15 is 5 above 10 and 13 is 3 above 10.\n" +
@@ -260,11 +281,17 @@ public class MathQuestions : MonoBehaviour {
 				"20\nmultiply by 10, then double\nExample: 20×4 = 40+40 = 80\nExample: 20×7 = 70+70 = 140\n\n\n";
 
 			break;
-		case 4:
+		case 4:	// %
 			temp = "What is the % symbol? It's modulus! Divide first and any remainder is your answer!\n\n" +
 				"In computing, the modulo operation finds the remainder after division of one number by another (sometimes called modulus). Given two positive numbers, a (the dividend) and n (the divisor), " +
 				"a modulo n (abbreviated as a mod n) is the remainder of the Euclidean division of a by n.\n\n";
 				
+			break;
+		case 5:	// sqaure root / radical symbol
+			temp = "In mathematics, a square root of a number a is a number y such that y² = a, in other words, a number y whose square (the result of multiplying the number by itself, or y * y) is a. \n\n " +
+				"For example, 4 and -4 are square roots of 16 because 4² = (-4)² = 16.Every non-negative real number a has a unique non-negative square root, called the principal square root, which is denoted by √a, where √ is called the radical sign or radix. \n\n" +
+				" For example, the principal square root of 9 is 3, denoted √9 = 3, because 32 = 3 ^ 3 = 9 and 3 is non-negative. The term whose root is being considered is known as the radicand. The radicand is the number or expression underneath the radical sign, " +
+				"in this example 9.The justification for taking out the square root of any number is this theorem to help simplify √a*b = √a * √b. The square root of a number is equal to the number of the square roots of each factor.";
 			break;
 		}
 		hintText.text = temp;
